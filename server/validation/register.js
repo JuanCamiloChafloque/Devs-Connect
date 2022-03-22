568d156bd5df5b73b1b44062e112b58d54aa7283
Camilo <camilochafloque@gmail.com>
2022-03-22 12:32:28 -0500

    Added form and states to register component

--- server/validation/register.js
+++ server/validation/register.js
@@ -16,12 +16,12 @@ module.exports = function validateRegisterInput(data) {
     errors.name = "Name field is required";
   }
 
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
-  }
-
   if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
   }
 
   if (Validator.isEmpty(data.password)) {
@@ -32,12 +32,12 @@ module.exports = function validateRegisterInput(data) {
     errors.password = "Password must be at least 6 characters";
   }
 
-  if (Validator.isEmpty(data.password2)) {
-    errors.password2 = "Confirm Password field is required";
-  }
-
   if (!Validator.equals(data.password, data.password2)) {
     errors.password2 = "Passwords must match";
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm Password field is required";
   }
 
   return {
