a3ff8452b7e052382ecb203a2d00bebffd83fa05
Camilo <camilochafloque@gmail.com>
2022-03-22 15:29:59 -0500

    Finished login and logout implementation with redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,4 +1,6 @@
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 import axios from "axios";
 
 //Register User
@@ -14,4 +16,46 @@ export const registerUser = (userData, history) => (dispatch) => {
         payload: err.response.data,
       });
     });
+};
+
+//Login User - Get JWT Token
+export const loginUser = (userData) => (dispatch) => {
+  axios
+    .post("/api/users/login", userData)
+    .then((res) => {
+      //Save Token to LocalStorage
+      const { token } = res.data;
+      //Set Token to LocalStorage
+      localStorage.setItem("jwtToken", token);
+      //Set Token to auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      //Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch((err) => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      });
+    });
+};
+
+// Set Logged-in user
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded,
+  };
+};
+
+// Log user out
+export const logoutUser = () => (dispatch) => {
+  //Remove token from LS
+  localStorage.removeItem("jwtToken");
+  //Remove the auth header for future requests
+  setAuthToken(false);
+  //Set current user to empty object which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
 };
