c6d038b05707b174ac4e3d0bcfd57fa4261accde
Camilo <camilochafloque@gmail.com>
2022-03-23 13:29:37 -0500

    Profile Header for Profile View finished

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -46,6 +46,25 @@ export const getProfiles = () => (dispatch) => {
     });
 };
 
+//Get Current Profile
+export const getProfileByHandle = (handle) => (dispatch) => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/handle/" + handle)
+    .then((res) => {
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      dispatch({
+        type: GET_PROFILE,
+        payload: null,
+      });
+    });
+};
+
 //Create Profile
 export const createProfile = (profileData, history) => (dispatch) => {
   axios
